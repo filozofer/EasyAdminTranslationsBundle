4a04f5ba2070dfb875abb70b3ded6a9018cdd31b
Jean-Philippe LAINE <jean-philippe.laine@niji.fr>
2017-01-13 12:02:55 +0100

    Repository - Using defaultTranslationLanguage variable and add random request

--- Repository/AbstractTranslatableEntityRepository.php
+++ Repository/AbstractTranslatableEntityRepository.php
@@ -10,20 +10,57 @@ use Doctrine\ORM\EntityRepository;
  */
 abstract class AbstractTranslatableEntityRepository extends EntityRepository
 {
-    public function findAll($languageIsoKey = null)
+    /**
+     * @param int $limit
+     * @param int $offset
+     * @return array
+     */
+    public function findForCurrentLanguage($limit = null, $offset = null)
     {
-        if (!empty($languageIsoKey)) {
-            $language = $this->getEntityManager()->getRepository('EasyAdminTranslationsBundle:Language')->findOneByIsoKey($languageIsoKey);
-            if (!empty($language)) {
-                $queryBuilder = $this->createQueryBuilder('e');
-                $queryBuilder
-                    ->join('e.translations', 'et')
-                    ->andWhere('et.language = :language')
-                    ->setParameter('language', $language)
-                ;
-                return $queryBuilder->getQuery()->getResult();
-            }
+        $queryBuilder = $this->getQueryBuilderForCurrentLanguage($limit, $offset);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param int $limit
+     * @return array
+     */
+    public function findRandomForCurrentLanguage($limit = null)
+    {
+        $queryBuilder = $this->getQueryBuilderForCurrentLanguage($limit);
+
+        $queryBuilder->orderBy('rand()');
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * Create query builder and include language request parameter for current language
+     *
+     * @param int $limit Optional parameter to set the maximum number of results to retrieve
+     * @param int $offset Optional parameter to sets the position of the first result to retrieve
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    private function getQueryBuilderForCurrentLanguage($limit = null, $offset = null)
+    {
+        global $defaultTranslationLanguage;
+
+        $queryBuilder = $this->createQueryBuilder('e');
+        $queryBuilder
+            ->join('e.translations', 'et')
+            ->andWhere('et.language = :language')
+            ->setParameter('language', $defaultTranslationLanguage)
+        ;
+
+        if ($limit !== null) {
+            $queryBuilder->setMaxResults($limit);
         }
-        return parent::findAll();
+
+        if ($offset !== null) {
+            $queryBuilder->setFirstResult($offset);
+        }
+
+        return $queryBuilder;
     }
 }
