557451ad7d3f8cbbfc2f12ec8d75c1918143cf5b
Maxime TUAL <maxime.tual@niji.fr>
2017-02-14 10:31:53 +0100

    Repository - Language
    * Method to find language like isokey

--- Repository/LanguageRepository.php
+++ Repository/LanguageRepository.php
@@ -3,11 +3,31 @@
 namespace EasyAdminTranslationsBundle\Repository;
 
 use \Doctrine\ORM\EntityRepository as DoctrineRepository;
+use EasyAdminTranslationsBundle\Entity\Language;
 
 /**
  * LanguageRepository
  */
 class LanguageRepository extends DoctrineRepository
 {
+
+    /**
+     * Find one language like is key ("en_" = "en_US" for example)
+     * @param string $isoKey
+     * @return Language
+     */
+    public function findOneLanguageLikeIsoKey($isoKey) {
+
+        // Build query
+        $queryBuilder = $this->createQueryBuilder('l');
+        $queryBuilder
+            ->andWhere('l.isoKey LIKE :isoKey')
+            ->setParameter('isoKey', '%' . $isoKey . '%')
+            ->setMaxResults(1)
+        ;
+
+        // Return results
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
 
 }
